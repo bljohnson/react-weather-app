e8946958496b486dac265ed1db12ae0c06ae8d70
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-11-25 16:42:11 -0600

    Add component properties

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,26 @@
 var Greeter = React.createClass({
+	getDefaultProps: function(){
+		return{
+			name: 'React',
+			message: 'This is the default message from the Greeter component.'
+		}
+	},
 	render: function(){
+		var name = this.props.name;
+		var message = this.props.message
 		return(
 			<div>
-				<h1>Hello React!</h1>
-				<p>This is from the Greeter component.</p>
+				<h1>Hello {name}!</h1>
+				<p>{message}</p>
 			</div>
 		);
 	}
 });
 
+var firstName = 'Bethany';
+var testMessage = 'This is a message.';
+
 ReactDOM.render(
-	<Greeter/>,
+	<Greeter name={firstName} message={testMessage} />,
 	document.getElementById('app')
 );
