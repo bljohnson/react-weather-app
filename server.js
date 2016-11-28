a43c317c1dff225cdfc20630a494d33860287495
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-11-28 15:48:13 -0600

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ var express = require('express');
 
 // create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// Express middleware - lets us do something with every request. Check if traffic is over http, since Open Weather Map API only operates on http not https.
+app.use(function(req, res, next){
+	if(req.headers['x-forwarded-proto'] === 'http'){
+		next();
+	} else{
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
 
 // serve public folder
 app.use(express.static('public'));
 
 // start server
-app.listen(3000, function() {
-	console.log('Now serving on port 3000');
+app.listen(PORT, function() {
+	console.log('Now serving on port ', PORT);
 });
