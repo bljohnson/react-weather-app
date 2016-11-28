2759042e43aec4bd41fff38b4fae5b889a8af8d8
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-11-27 22:58:37 -0600

    Add page components

--- app/app.jsx
+++ app/app.jsx
@@ -3,13 +3,18 @@ var ReactDOM = require('react-dom');
 // ES6 object destructuring syntax - creates variable called Route and sets it equal to route property of react-router module, and etc.
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+var Examples = require('Examples');
 
 ReactDOM.render(
 	// set up Router
 	<Router history={hashHistory}>
 		{/* define individual routes */}
 		<Route path="/" component={Main}>
-
+			<Route path="about" component={About}/>
+			<Route path="examples" component={Examples}/>
+			<IndexRoute component={Weather}/>
 		</Route>
 	</Router>,
 	document.getElementById('app')
