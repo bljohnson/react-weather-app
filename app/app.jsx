eedbc06e0e20ffbf3d16b43ba7ad0d00a15fd767
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-11-27 22:06:57 -0600

    Add React router

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+// ES6 object destructuring syntax - creates variable called Route and sets it equal to route property of react-router module, and etc.
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
 
 ReactDOM.render(
-	<h1>React Boilerplate Project!</h1>,
+	// set up Router
+	<Router history={hashHistory}>
+		{/* define individual routes */}
+		<Route path="/" component={Main}>
+
+		</Route>
+	</Router>,
 	document.getElementById('app')
 );
---
+++ app/components/Nav.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+// create Nav component
+var Nav = React.createClass({
+	render: function(){
+		return(
+			<h2>Nav Component</h2>
+		);
+	}
+});
+
+module.exports = Nav;
