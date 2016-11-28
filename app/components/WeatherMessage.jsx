b86e0a3083ef81d7f486ad352d789c0c28a53f5e
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-11-27 23:27:17 -0600

    Add WeatherForm and WeatherMessage components

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var WeatherMessage = React.createClass({
+	render: function(){
+		return(
+			<h3>It's 44 F in Minneapolis.</h3>
+		)
+	}
+});
+
+module.exports = WeatherMessage;
